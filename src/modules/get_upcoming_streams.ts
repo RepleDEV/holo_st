f68b94c8b1528648f7dd5538aa670625b158f9bb
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2021-01-29 10:45:13 +0700

    Might regret this change

--- src/modules/get_upcoming_streams.ts
+++ src/modules/get_upcoming_streams.ts
@@ -30,7 +30,13 @@ async function get_html(url: string, browser_p?: puppeteer.Browser): Promise<str
     await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36");
     await page.goto(url, { waitUntil: "networkidle0" });
     const data = await page.evaluate(() => document.querySelector("*")?.outerHTML) || "";
-    if (!browser_p)await browser.close();
+
+    // Don't close the browser if it's imported but close the page.
+    if (browser_p) {
+        await page.close();
+    } else {
+        await browser.close();
+    }
 
     return data;
 }
